c56417144a4d23357cb62059c74f44abe6d42a4a
Dmitry Zhmurko <zhmurko@gmail.com>
2018-03-17 12:21:09 +0200

    step 9 check isReady to start

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,7 +6,17 @@ import (
 
 // Router register necessary routes and returns an instance of a router.
 func Router(buildTime, commit, release string) *mux.Router {
+    isReady := &atomic.Value{}
+    isReady.Store(false)
+    go func() {
+        log.Printf("Readyz probe is negative by default...")
+        time.Sleep(2 * time.Second)
+        isReady.Store(true)
+        log.Printf("Readyz probe is positive.")
+    }()
     r := mux.NewRouter()
     r.HandleFunc("/status", status(buildTime, commit, release)).Methods("GET")
+    r.HandleFunc("/healthz", healthz)
+    r.HandleFunc("/readyz", readyz(isReady))
     return r
 }
